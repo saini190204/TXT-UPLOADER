9b42e3b2e039e519ecf4cc9d6b5229b6d2b240c5
GODARA7 <subhashgodara237057@gmail.com>
2025-01-09 10:58:16 +0530

    Update and rename config.py to vars.py

---
+++ vars.py
@@ -0,0 +1,9 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "28328736"))
+API_HASH = environ.get("API_HASH", "802254a44896baa87f3083b7af36b2e5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7576398657:AAHkCx9Z3ZC3FZcBa5s92ghVVEnh57WQpGc")
